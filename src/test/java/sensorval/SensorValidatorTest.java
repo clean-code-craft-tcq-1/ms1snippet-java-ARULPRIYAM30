d3aa5948688c81937222b17b7f91d7e9e5469366
ARULPRIYAM30 <77484841+ARULPRIYAM30@users.noreply.github.com>
2021-03-25 20:36:58 +0530

    Update SensorValidatorTest.java

--- src/test/java/sensorval/SensorValidatorTest.java
+++ src/test/java/sensorval/SensorValidatorTest.java
@@ -2,10 +2,9 @@ package sensorval;
 
 import static org.junit.Assert.*;
 import jdk.nashorn.internal.AssertsEnabled;
-
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
-
 import org.junit.Test;
 
 public class SensorValidatorTest 
@@ -24,4 +23,32 @@ public class SensorValidatorTest
         List<Double> currents = Arrays.asList(readings);
         assertFalse(SensorValidator.validateCurrentreadings(currents));
     }
+    
+    @Test
+    public void reportsErrorWhenCurrentAllValuesNull()
+    {
+       List<Double> readings = null;
+       assertFalse(SensorValidator.validateCurrentreadings(readings));
+    }
+    @Test
+    public void reportsErrorWhenCurrentHasNull()
+    {
+        Double[] readings = {0.03, 0.03, null, 0.33};
+        List<Double> currents = Arrays.asList(readings);
+        assertFalse(SensorValidator.validateCurrentreadings(currents));
+    }
+    
+    @Test
+    public void reportsErrorWhenSOCAllValuesNull()
+    {
+       List<Double> readings = null;
+       assertFalse(SensorValidator.validateSOCreadings(readings));
+    }
+    @Test
+    public void reportsErrorWhenSOCHasNull()
+    {
+        Double[] readings = {0.03, 0.03, null, 0.33};
+        List<Double> socs = Arrays.asList(readings);
+        assertFalse(SensorValidator.validateSOCreadings(socs));
+    } 
 }
