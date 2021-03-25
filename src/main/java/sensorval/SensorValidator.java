6e4253afef6944dff3ba204921e295810f69b471
ARULPRIYAM30 <77484841+ARULPRIYAM30@users.noreply.github.com>
2021-03-25 20:36:54 +0530

    Update SensorValidator.java

--- src/main/java/sensorval/SensorValidator.java
+++ src/main/java/sensorval/SensorValidator.java
@@ -4,28 +4,36 @@ import java.util.List;
 
 public class SensorValidator 
 {
-    public static boolean _give_me_a_good_name(double value, double nextValue, double maxDelta) {
-        if(nextValue - value > maxDelta) {
-            return false;
-        }
-        return true;
+	static final double MAX_SOC_VALUE=0.05;
+	static final double MAX_CURRENT_VALUE=0.1;
+	
+    private static boolean checkSuddenJump(double value, double nextValue, double maxDelta) {
+    	return (nextValue - value > maxDelta);
     }
     public static boolean validateSOCreadings(List<Double> values) {
-        int lastButOneIndex = values.size() - 1;
-        for(int i = 0; i < lastButOneIndex; i++) {
-            if(!_give_me_a_good_name(values.get(i), values.get(i + 1), 0.05)) {
-            return false;
-            }
-        }
-        return true;
+    	return isValid(values)? validateReadings(values,MAX_SOC_VALUE):false;
     }
-    public static boolean validateCurrentreadings(List<Double> values) {
-        int lastButOneIndex = values.size() - 1;
+	public static boolean validateCurrentreadings(List<Double> values) {
+    	return isValid(values)? validateReadings(values,MAX_CURRENT_VALUE):false;
+    }
+    private static boolean isValid(List<Double> values) {
+		return notNull(values) && noNullContent(values);
+	}
+	private static boolean noNullContent(List<Double> values) {
+		return !values.contains(null);
+	} 
+    private static boolean notNull(List<Double> values) {
+    	return (values!=null && values.size()>0);
+    }
+    
+  public static boolean validateReadings(List<Double> values,double maxInterval){
+    	int lastButOneIndex = values.size() - 1;
         for(int i = 0; i < lastButOneIndex; i++) {
-            if(!_give_me_a_good_name(values.get(i), values.get(i + 1), 0.1)) {
+            if(checkSuddenJump(values,i,maxInterval)) {
             return false;
             }
         }
         return true;
     }
 }
+
